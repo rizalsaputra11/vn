cc1af92a57aba1ed5ef72c64326ad9feea7f51a7
razzv <rizalsaputra11@outlook.com>
2025-11-16 09:49:37 +0700

    Update ip-make.py

--- ip-make.py
+++ ip-make.py
@@ -1,4 +1,4 @@
-ips = [f"10.10.10.{i}" for i in range(2, 255)]
+ips = [f"10.0.2.{i}" for i in range(2, 255)]
 with open("ips.txt", "w") as f:
     f.write("\n".join(ips))
 print(f"Wrote {len(ips)} IPs to ips.txt")
